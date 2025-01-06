18b37903e2b979b1c534471079cbe45b954e7742
Steve Stuart <steve.stuart@cyone.com>
2025-01-06 11:32:46 -0500

    Create bootstrap.ps1

---
+++ bootstrap.ps1
@@ -0,0 +1,16 @@
+#install scoop
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+#install git via scoop if git not found
+if(-not  (get-command git)){scoop install git}
+
+#install task
+scoop bucket add main
+scoop install main/task
+$ENV:TASK_X_REMOTE_TASKFILES=1
+
+
+#install helm
+task -t https://raw.githubusercontent.com/CyoneDev/commonTaskfiles/refs/heads/main/installs/helm.taskfile.yaml -y install
+#install pwsh 7
+task -t https://raw.githubusercontent.com/CyoneDev/commonTaskfiles/refs/heads/main/installs/pwsh.taskfile.yaml -y install
