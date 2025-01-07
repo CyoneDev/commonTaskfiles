37c4d39f23a89717e0e9f07417520ea9d4653598
Steve Stuart <steve.stuart@cyone.com>
2025-01-07 14:46:58 -0500

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,11 @@
 #run with `Invoke-Expression -Command $(Invoke-WebRequest https://raw.githubusercontent.com/CyoneDev/commonTaskfiles/refs/heads/main/bootstrap.ps1)`
+#install pwsh 7
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = '& Set-Item -Path Env:TASK_X_REMOTE_TASKFILES -Value 1;task -t https://raw.githubusercontent.com/CyoneDev/commonTaskfiles/refs/heads/main/installs/pwsh.taskfile.yaml -y install'
+  Start-Process powershell -Verb runAs -ArgumentList $arguments -wait
+  Break
+}
 #install scoop
 if(-not (get-command scoop -erroraction silentlycontinue)){
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
@@ -17,13 +24,7 @@ $ENV:TASK_X_REMOTE_TASKFILES=1
 
 #install helm
 task -t https://raw.githubusercontent.com/CyoneDev/commonTaskfiles/refs/heads/main/installs/helm.taskfile.yaml -y install
-#install pwsh 7
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
-{  
-  $arguments = '& Set-Item -Path Env:TASK_X_REMOTE_TASKFILES -Value 1;task -t https://raw.githubusercontent.com/CyoneDev/commonTaskfiles/refs/heads/main/installs/pwsh.taskfile.yaml -y install'
-  Start-Process powershell -Verb runAs -ArgumentList $arguments -wait
-  Break
-}
+
 #install docker desktop
 if( -not(get-command docker -erroraction silentlycontinue)){
 invoke-expression -command $(invoke-webrequest https://raw.githubusercontent.com/CyoneDev/commonTaskfiles/refs/heads/main/scripts/install-docker-desktop_win.ps1)
